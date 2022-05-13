d5ee8dff40fa0ee2b1651b726e3286c5a2ef0ca2
Kaustubh <ykaustubh22@gmail.com>
2022-05-14 04:11:46 +0530

    Github Pages 404 r-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,20 +5,21 @@ import Contact from './Pages/Contact';
 import About from './Pages/About';
 import Projects from './Pages/Projects';
 import Navbar from './Navbar';
+import NotFound from './NotFound';
 import './App.css';
 
 import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
 
 const App= () => {
     return (<>
-        <Router>
+        <Router basename="/ReactWebDemo">
             <Navbar/>
             <Routes>
                 <Route path= "/" element= {<Home/>}/>
                 <Route path= "/about" element= {<About/>}/>
                 <Route path= "/contact" element= {<Contact/>}/>
                 <Route path= "/projects" element= {<Projects/>}/> 
-                <Route path = '*' element = {<Home/>}/>             
+                <Route path = '*' element = {<NotFound/>}/>             
             </Routes> 
         </Router>
         </>
